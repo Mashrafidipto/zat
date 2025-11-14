5558d0f92ff41f3e4c682747a8cafe1eb4f93bf4
MashrafiDipto <mashrafidipto@gmail.com>
2025-11-14 23:00:23 +0600

    Making the zat work

--- src/main.zig
+++ src/main.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
 const fs = std.fs;
 const env = std.process;
+const Reader = std.io.Reader;
 
 const MAX_LENGTH: usize = 1024;
 
@@ -8,32 +9,29 @@ var stdout_buffer = [_]u8{0} ** MAX_LENGTH;
 var stdout_writer = fs.File.stdout().writer(&stdout_buffer);
 pub var stdout = &stdout_writer.interface;
 
+var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+const alocator = gpa.allocator();
+
 pub fn main() !void {
-    _ = try get_file_reader();
-}
-pub fn mainOld() !void {
-    var file_buffer = [_]u8{0} ** MAX_LENGTH;
-    const file_location = try fs.cwd().openFile("build.zig.zon", .{ .mode = .read_only });
-    var file_interface = file_location.reader(&file_buffer);
-    const file_reader = &file_interface.interface;
-
-    while (try file_reader.takeDelimiter('\n')) |line| {
-        try stdout.print("{s}\n", .{line});
-        try stdout.flush();
-    }
-}
-
-pub fn get_file_reader() !?std.io.Reader {
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const alocator = gpa.allocator();
-
     const agrv = try env.argsAlloc(alocator);
     defer env.argsFree(alocator, agrv);
 
-    for (agrv) |line| {
+    const file = if (agrv.len > 1) try fs.cwd().openFile(agrv[1], .{ .mode = .read_only }) else fs.File.stdin();
+
+    //colsing file if not stdin()
+    defer {
+        if (agrv.len == 1) {
+            file.close();
+        }
+    }
+    var file_buffer = [_]u8{0} ** MAX_LENGTH;
+    var file_reader = file.reader(&file_buffer);
+    try print_contents(&file_reader.interface);
+}
+
+pub fn print_contents(file_interface: *Reader) !void {
+    while (try file_interface.takeDelimiter('\n')) |line| {
         try stdout.print("{s}\n", .{line});
     }
     try stdout.flush();
-
-    return null;
 }
